caac8c85b05af0d74091bc8cd3f41f7485e6632d
Komal Suthar <komalsuthar19122000@gmail.com>
2026-06-26 00:25:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
   subnet_id     = "subnet-0b0279adec6770876"
   tags          = {
-      Name = Komal
-      Owner = Komal Suthar
-      Environment = dev
+      Name = "Komal"
+      Owner = "Komal Suthar"
+      Environment = "dev"
   }
 }
